106b9a6106c876733fb5ebecacb757380fdca505
AndreuPuchades <puchadesandreupascual@gmail.com>
2025-03-05 21:43:59 +0100

    Change test

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
                     <div class="flex justify-between items-center">
                         <div class="text-2xl font-bold text-gray-800">
-                            Company Lista
+                            Company List
                         </div>
                         <a href="{{ route('companies.create') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:border-blue-800 focus:ring focus:ring-blue-300 disabled:opacity-25 transition">
                             <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
